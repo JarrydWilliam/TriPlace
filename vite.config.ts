2af8d78b50449a6d3b6e6d5a80a34c2eea240cd2
JarrydWilliam <134390764+JarrydWilliam@users.noreply.github.com>
2026-05-01 14:47:25 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,26 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { resolve } from "path";
 
 export default defineConfig({
-    plugins: [
-          react(),
-          runtimeErrorOverlay(),
-        ],
-    resolve: {
-          alias: {
-                  "@": resolve(process.cwd(), "client", "src"),
-                  "@shared": resolve(process.cwd(), "shared"),
-                  "@assets": resolve(process.cwd(), "attached_assets"),
-          },
-    },
-    root: resolve(process.cwd(), "client"),
-    build: {
-          outDir: resolve(process.cwd(), "dist/public"),
-          emptyOutDir: true,
-    },
-    server: {
-          fs: {
-                  strict: true,
-                  deny: ["**/.*"],
-          },
-    },
+      plugins: [
+              react(),
+              runtimeErrorOverlay(),
+            ],
+      resolve: {
+              alias: {
+                        "@": resolve(process.cwd(), "client", "src"),
+                        "@shared": resolve(process.cwd(), "shared"),
+                        "@assets": resolve(process.cwd(), "attached_assets"),
+              },
+      },
+      root: resolve(process.cwd(), "client"),
+      build: {
+              outDir: resolve(process.cwd(), "dist"),
+              emptyOutDir: true,
+      },
+      server: {
+              fs: {
+                        strict: true,
+                        deny: ["**/.*"],
+              },
+      },
 });
